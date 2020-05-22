5e196f0c45dadf777c1fbb720fdf5c66ecd8bf07
Fabian <draizfabian@gmail.com>
2020-05-23 01:00:55 +0300

    normalized data

---
+++ play.py
@@ -0,0 +1,31 @@
+import librosa
+import numpy as np # linear algebra
+import pandas as pd
+from pydub import AudioSegment
+import re
+
+
+"""
+t1 = 0#Works in milliseconds
+t2 =  1000
+Audio = AudioSegment.from_wav("test.wav")
+
+df=pd.read_csv("esc50.csv")
+selected=['airplane', 'breathing',  'car_horn', 'cat',  'chirping_birds', 'church_bells', 'clapping',
+    'coughing',   'crickets','crying_baby', 'dog', 'door_wood_creaks', 'door_wood_knock',  'engine',
+     'fireworks', 'footsteps',  'glass_breaking','hand_saw', 'helicopter',  'insects',  'laughing',
+      'mouse_click',  'pouring_water', 'rain', 'rooster','siren', 'sneezing','thunderstorm',  'train','wind']
+df=df.loc[df['category'].isin(selected)]
+path='audio/44100/'
+for filename in df.filename:
+    t1 = 0#Works in milliseconds
+    t2 =  1000
+    Audio = AudioSegment.from_wav(path+filename)
+    for i in range(5):
+        newAudio = Audio[t1:t2]
+        t1=t1+1000
+        t2=t2+1000
+        name=re.sub(".wav","",filename)
+        newAudio.export('audio/segmented/'+name+str(i)+".wav", format="wav")
+
+"""
