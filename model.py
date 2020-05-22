53daf04e7463312a9546e61784eebb67460d845c
Fabian <draizfabian@gmail.com>
2020-05-22 19:35:47 +0300

    pic abort

--- model.py
+++ model.py
@@ -16,9 +16,7 @@ from sklearn.preprocessing import OneHotEncoder
 import pickle
 from tqdm import tqdm
 from PIL import Image
-from PIL import Image
 from numpy import asarray
-
 from keras.utils import to_categorical
 
 df=pd.read_csv("esc50.csv")
@@ -27,49 +25,51 @@ selected=['airplane', 'breathing',  'car_horn', 'cat',  'chirping_birds', 'churc
  'fireworks', 'footsteps',  'glass_breaking','hand_saw', 'helicopter',  'insects',  'laughing',
   'mouse_click',  'pouring_water', 'rain', 'rooster','siren', 'sneezing','thunderstorm',  'train','wind']
 df=df.loc[df['category'].isin(selected)]
-X=[]
+
 path="audio/44100/"
 
-pickle_in=open("y.pickle","rb")#loading them back
-Y=pickle.load(pickle_in)
-#print(Y)
-print(Y)
-print(Y.shape)
-Y=np.concatenate((Y,Y),axis=0)
-print(Y)
-print(Y.shape)
-LabelEncoder=LabelEncoder()
-Y=LabelEncoder.fit_transform(Y)
-Y=to_categorical(Y)
 
+#print(Y)
+
+Y=[]
 X=[]
 x_path='spectrograms/'
 for i in df.filename:
+    cl=df.loc[df['filename']==i,'category'].iloc[0]
+    Y.append(cl)
     img=Image.open(x_path+i+'.png')
+    img_array=asarray(img)
+    X.append(img_array)
+for i in df.filename:
+    cl=df.loc[df['filename']==i,'category'].iloc[0]
+    Y.append(cl)
+    img=Image.open(x_path+'morphed/'+i+'.png')
     img_array=asarray(img)
     X.append(img_array)
 
 
-print("X:",X[0])
-X=np.array(X).reshape(-1,640, 480, 1)
-##train_x,test_x,train_y,test_y = train_test_split(X, Y, test_size = 0.1, random_state=5, shuffle = True)
-#input_1,input_2=X[0].shape
+X=np.array(X)
+print("X:",X[0].shape)
+
+LabelEncoder=LabelEncoder()
+Y=LabelEncoder.fit_transform(Y)
 print("label shape",Y.shape)
 #print("input_1,input_2",input_1,input_2)
 print("X shape",X.shape)
+train_x,test_x,train_y,test_y = train_test_split(X, Y, test_size = 0.1, random_state=5, shuffle = True)
+input_1,input_2,input_3=X[0].shape
 
-"""
 model=Sequential()
 
-model.add(Conv1D(32,kernel_size=(3,3) ,activation="relu",input_shape=(input_1,input_2)))
+model.add(Conv2D(32,kernel_size=(3,3) ,activation="relu",input_shape=(input_1,input_2,input_3)))
 model.add(Dropout(0,2))
-model.add(Conv1D(32,kernel_size=(3,3) ,activation="relu"))
+model.add(Conv2D(32,kernel_size=(3,3) ,activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv1D(64,kernel_size=(3,3) ,activation="relu"))
+model.add(Conv2D(64,kernel_size=(3,3) ,activation="relu"))
 model.add(Dropout(0,2))
-model.add(Conv1D(64,kernel_size=(3,3) ,activation="relu"))
+model.add(Conv2D(64,kernel_size=(3,3) ,activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv1D(128,kernel_size=(3,3) ,activation="relu"))
+model.add(Conv2D(128,kernel_size=(3,3) ,activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(64, activation = 'relu'))
@@ -88,4 +88,3 @@ model.fit(train_x, train_y, batch_size = 32 ,epochs=30,validation_split=0.1)
 # save the model to disk
 filename = 'model1.sav'
 pickle.dump(model, open(filename, 'wb'))
-"""
