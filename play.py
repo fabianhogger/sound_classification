2acca9f4a818e7c8cce2664448d7a3b29a7fd694
Fabian <draizfabian@gmail.com>
2020-05-23 13:46:27 +0300

    0.1 sec

--- play.py
+++ play.py
@@ -9,7 +9,7 @@ import re
 t1 = 0#Works in milliseconds
 t2 =  1000
 Audio = AudioSegment.from_wav("test.wav")
-
+"""
 df=pd.read_csv("esc50.csv")
 selected=['airplane', 'breathing',  'car_horn', 'cat',  'chirping_birds', 'church_bells', 'clapping',
     'coughing',   'crickets','crying_baby', 'dog', 'door_wood_creaks', 'door_wood_knock',  'engine',
@@ -19,13 +19,11 @@ df=df.loc[df['category'].isin(selected)]
 path='audio/44100/'
 for filename in df.filename:
     t1 = 0#Works in milliseconds
-    t2 =  1000
+    t2 =  100
     Audio = AudioSegment.from_wav(path+filename)
-    for i in range(5):
+    for i in range(50):
         newAudio = Audio[t1:t2]
-        t1=t1+1000
-        t2=t2+1000
+        t1=t1+100
+        t2=t2+100
         name=re.sub(".wav","",filename)
-        newAudio.export('audio/segmented/'+name+str(i)+".wav", format="wav")
-
-"""
+        newAudio.export('audio/segmented100/'+name+str(i)+".wav", format="wav")
