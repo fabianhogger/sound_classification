b73d5786139bf19d71d2f70372c17ac67df1a3b9
Fabian <draizfabian@gmail.com>
2020-05-23 14:47:23 +0300

    saved data as audio

--- model.py
+++ model.py
@@ -22,22 +22,38 @@ selected=['airplane', 'breathing',  'car_horn', 'cat',  'chirping_birds', 'churc
   'mouse_click',  'pouring_water', 'rain', 'rooster','siren', 'sneezing','thunderstorm',  'train','wind']
 df=df.loc[df['category'].isin(selected)]
 
-path="audio/segmented/"
+path="audio/segmented100/"
 X=[]
 Y=[ ]
 for filename in df.filename:
     class_=df.loc[df['filename']==filename,'category'].iloc[0]
     name=re.sub(".wav","",filename)
-    for i in range(5):
+    for i in range(50):
         tmp=librosa.load(path+name+str(i)+'.wav',sr=44100)
         X.append(tmp)
         Y.append(class_)
 print(X)
 X=np.array(X)
 print("X shape: ",X.shape)
+print("X[0] shape: ",X[0].shape)
+
+pickle_out=open("X3.pickle","wb")
+pickle.dump(X,pickle_out)
+pickle_out.close()
 Y=np.array(Y)
+
+
+
 print("Y shape: ",Y.shape)
 print(Y)
+
+LabelEncoder=LabelEncoder()
+Y=LabelEncoder.fit_transform(Y)
+
+pickle_out=open("Y3.pickle","wb")
+pickle.dump(Y,pickle_out)
+pickle_out.close()
+
 """
 X=np.array(X)
 #print("X:",X[0].shape)
@@ -60,8 +76,12 @@ print("1 sample :",X[0])
 input_1=1025
 input_2=431
 train=train.reshape(1800,1025,431,1)
-
-
+"""
+print("X[0] : ",X[0])
+X=X.reshape(1,60000,2,1)
+print("reshaped: ",X.shape)
+print("X[0] after reshape : ",X[0])
+"""
 model=Sequential()
 
 model.add(Conv2D(32,kernel_size=(3,3) ,activation="relu",input_shape=(input_1,input_2,1)))
