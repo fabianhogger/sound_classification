52130ec9436bdec66ade6d21da6f158487db26d9
Fabian <32894026+fabianhoegger@users.noreply.github.com>
2020-05-24 22:18:19 +0300

    Update model.py

--- model.py
+++ model.py
@@ -15,14 +15,29 @@ import pickle
 import re
 from python_speech_features import mfcc
 
-#from tqdm import tqdm
 """
-df=pd.read_csv("esc50.csv")
+##DATA CREATION CODE ,UNCOMMENT THIS SECTION TO RECTREATE THE DATA
+
+df=pd.read_csv("esc50.csv")#open csv containing filenames and classes
 selected=['airplane', 'breathing',  'car_horn', 'cat',  'chirping_birds', 'church_bells', 'clapping',
 'coughing',   'crickets','crying_baby', 'dog', 'door_wood_creaks', 'door_wood_knock',  'engine',
  'fireworks', 'footsteps',  'glass_breaking','hand_saw', 'helicopter',  'insects',  'laughing',
   'mouse_click',  'pouring_water', 'rain', 'rooster','siren', 'sneezing','thunderstorm',  'train','wind']
-df=df.loc[df['category'].isin(selected)]
+
+df=df.loc[df['category'].isin(selected)]# selected only classes listed above
+
+
+for filename in df.filename:
+    t1 = 0    #Works in milliseconds ,move 100ms every time ,i goes up to 250 because training files are 5s long
+    t2 =  100
+    Audio = AudioSegment.from_wav(path+filename)
+    for i in range(250):
+        newAudio = Audio[t1:t2]
+        t1=t1+100
+        t2=t2+100
+        name=re.sub(".wav","",filename)
+        newAudio.export('audio/segmented10/'+name+str(i)+".wav", format="wav")
+
 X=[]
 Y=[]
 _min=200000
@@ -30,38 +45,32 @@ rate=44100
 _max=-1
 step=int(rate/10)
 rand1=0
-for i in range(50000):
-    filename=np.random.choice(df['filename'])
-    class_=df.loc[df['filename']==filename,'category'].iloc[0]
-    #name=re.sub(".wav","",filename)
-    #rand1==np.random.randint(0,49)
-    rate,wav=wavfile.read('audio/44100/'+filename)
-    rand_index=np.random.randint(0,wav.shape[0]-step)
-    sample=wav[rand_index:rand_index+step]#ra
-    mf=mfcc(sample,samplerate=44100, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=1103, lowfreq=0, highfreq=None, preemph=0.97, ceplifter=22, appendEnergy=True)
-    _min=min(np.amin(mf),_min)
-    _max=max(np.amax(mf),_max)
-    X.append(mf)
-    Y.append(class_)
-print(X)
-X=np.array(X)
-X=(X-_min)/(_max-_min)
+for i in range(60000):
+X=[]
+Y=[]
+for filename in df.filename:
+    class_=df.loc[df['filename']==filename,'category'].iloc[0]#find  class of audio file
+    name=re.sub(".wav","",filename) #remove wav part
+    for i in range(50):
+        tmp,rate=librosa.load("audio/segmented100/"+name+str(i)+'.wav',sr=44100) #load the 100ms segments
+        mf=librosa.feature.mfcc(tmp,rate) #calculate mfcc
+        X.append(mf)
+        Y.append(class_)
+
 print("X shape: ",X.shape)
 print("X[0] shape: ",X[0].shape)
 
-pickle_out=open("X8.pickle","wb")
+pickle_out=open("X6.pickle","wb") #save calculations
 pickle.dump(X,pickle_out)
 pickle_out.close()
 Y=np.array(Y)
-
-
 print("Y shape: ",Y.shape)
 print(Y)
 
 LabelEncoder=LabelEncoder()
-Y=LabelEncoder.fit_transform(Y)
+Y=LabelEncoder.fit_transform(Y)# map the strings to int
 
-pickle_out=open("Y8.pickle","wb")
+pickle_out=open("Y6.pickle","wb") #save calculations
 pickle.dump(Y,pickle_out)
 pickle_out.close()
 """
@@ -79,9 +88,8 @@ print("x shape",X.shape)
 print("SAMPLE shape",X[0].shape)
 
 print("X[0] : ",X[0])
-X=X.reshape(60000,20,9,1)
-#print("reshaped: ",X.shape)
-#print("X[0] after reshape : ",X[0])
+X=X.reshape(60000,20,9,1) #reshape to 4d array
+
 
 model=Sequential()
 
@@ -97,11 +105,11 @@ model.add(Dense(64,activation='relu'))
 model.add(Dense(30,activation='softmax'))
 
 model.summary()
-# compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Training and Evaluation of the model
-model.fit(X, Y, batch_size = 30 ,epochs=30,validation_split=0.1)
-model.save("model4.sav")
+model.fit(X, Y, batch_size = 1 ,epochs=10,validation_split=0.1)
+model.save("model3.sav")# save model
+
+
